71f8fd79a51ca1845dfc9d6ef1a29a82ac3dbb8a
isamurray <imurr054@uottawa.ca>
2016-11-23 12:48:46 -0500

    added class Player

---
+++ CardGameProject/Player.cpp
@@ -0,0 +1,70 @@
+#include "Player.h"
+
+Player:: Player(string &playerName) {											//quoi faire avec la reference
+	name = playerName;
+	numCoins = 0;
+	maxNumChains = 2;
+	chain = new Chain[2];
+	chain[0] = NULL;
+	chain[1] = NULL;
+
+}
+
+
+string Player :: getName() {
+	return name;
+}
+
+int Player:: getNumCoins() {
+	return numCoins;
+}
+
+Player& Player:: operator +=(int i) {
+	numCoins = numCoins + i;
+	return(*this);
+}
+
+int Player:: getMaxNumChains() {
+	return maxNumChains;
+}
+
+int Player::getNumChains() {
+	int count=0;
+	for (int i = 0; i < maxNumChains; i++) {
+		if (chain[i] != NULL)
+			count++;
+	}
+	return count;
+
+}
+
+
+ Chain& Player:: operator[](int i) {															//implementer?
+	 return chain[i];
+}
+
+
+void Player:: buyThirdChain() {
+	if (numCoins < 2)
+		throw NotEnoughCoins();
+	else {
+		numCoins = numCoins - 2;
+		*(chain + 2) = new Chain;											//la troisieme chaine est elle dans le bon ordre
+
+		chain[2] = NULL;
+	}
+
+}
+
+void Player::printHand(ostream&, bool) {
+
+
+
+}//prints the top card of the player's hand (with
+								//argument false) or all of the player's hand (with 								//argument true) to the supplied ostream.
+
+Player:: Player(const istream&, CardFactory*) {
+
+
+
+}//constructor that accepts an istream and reconstruct the Player from file
---
+++ CardGameProject/Player.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include "CardFactory.h"
+#include <exception>
+
+using namespace std;
+
+class Player {
+	string name;
+	int numCoins;
+	int maxNumChains;
+	Chain* chain;
+
+public:
+	Player(std::string&);			// constructor that creates a Player with a given name.
+	string getName();				// get the name of the player.
+	int getNumCoins();				// get the number of coins currently held by the player.
+	Player& operator+=(int);		// add a number of coins
+	int getMaxNumChains();			// returns either 2 or 3.
+	int getNumChains();				// returns the number of non - zero chains
+	Chain& operator[](int i);		// returns the chain at position i.
+	void buyThirdChain();			//adds an empty third chain to the player for two coins
+	void printHand(ostream&, bool); //prints the top card of the player's hand (with
+									//argument false) or all of the player's hand (with 									//argument true) to the supplied ostream.
+	Player(const istream&, CardFactory*); //constructor that accepts an istream and reconstruct the Player from file};class NotEnoughCoins : public exception
+{
+	virtual const char* what() const throw()
+	{
+		return "Not enough coins to buy a thirs chain";
+	}
+};
